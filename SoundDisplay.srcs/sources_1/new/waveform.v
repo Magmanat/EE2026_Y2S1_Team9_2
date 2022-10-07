7276ab05920d6cc194c4be92b0c663f4ee6a6729
Prince Pereira <74498975+Magmanat@users.noreply.github.com>
2022-10-07 23:53:08 +0800

    removed conflict lines

--- SoundDisplay.srcs/sources_1/new/waveform.v
+++ SoundDisplay.srcs/sources_1/new/waveform.v
@@ -35,7 +35,6 @@ clock_divider  custom_clock(CLK, custom_clk_speed, custom_clk);
 
 always @ (posedge custom_clk) begin
     if (selected == 0) begin
-<<<<<<< HEAD
         num = mic_in[10:0] / 11'd64;
         if (mic_in[11]) begin
             waveform[5:0] = 6'd32 - num;
@@ -44,39 +43,6 @@ always @ (posedge custom_clk) begin
             waveform[5:0] = 6'd63 - num;
         end
         waveform = waveform << 6;      
-=======
-//    //take reading 96 times a second
-//        if (counter == 207) begin
-//            counter <= 0;
-//            //if counter increment, update x and y            
-//            waveform_y <= mic_in / 64;
-//            if (waveform_x == 95) begin
-//                waveform_x <= 0;
-//            end
-//            else begin
-//                waveform_x <= waveform_x + 1;
-//            end
-//        end        
-//        else begin
-//            counter <= counter + 1;
-//        end
-    //take reading 96 times a second   
-    /////////////////////////////////////
-//        num = mic_data[11:6];
-//        transnum = num > `HEIGHT/2 ? num : `HEIGHT - num; // get value above x-axis
-//        if (yreflect < transnum && yreflect > `HEIGHT - transnum) begin
-//            ygre = (transnum <= `HEIGHT/2 + greRange && transnum >= `HEIGHT/2 - greRange);
-    /////////////////////////////////////
-        waveform[5:0] = mic_in / 65;
-        waveform = waveform << 6;
-        // y <= mic_in / 64;      
-        // if (x == 95) begin
-        //     x <= 0;
-        // end
-        // else begin
-        //     x <= x + 1;
-        // end            
->>>>>>> main
     end
 end
 
