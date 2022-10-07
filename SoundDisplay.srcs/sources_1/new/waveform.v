8842232782a29c415e52d0df45b167b512af0eb8
Prince Pereira <74498975+Magmanat@users.noreply.github.com>
2022-10-07 23:38:05 +0800

    made waveform slowly appear

--- SoundDisplay.srcs/sources_1/new/waveform.v
+++ SoundDisplay.srcs/sources_1/new/waveform.v
@@ -21,25 +21,31 @@
 
 
 module waveform(
-    input clk20k,
+    input CLK,
     input [1:0] selected,
     input [11:0] mic_in,
     output reg [(95 * 6) - 1:0] waveform
 );
 
-reg [4:0] num;
+reg [5:0] num;
+reg [7:0] counter;
+reg [31:0] custom_clk_speed = 31'd1000000;
+wire custom_clk;
+clock_divider  custom_clock(CLK, custom_clk_speed, custom_clk);
 
-always @ (posedge clk20k) begin
+always @ (posedge custom_clk) begin
     if (selected == 0) begin
+        num = mic_in[10:0] / 11'd64;
         if (mic_in[11]) begin
-            num = mic_in[10:0] / 64;
-            waveform[5:0] = 31 + num;
+            waveform[5:0] = 6'd32 - num;
         end     
         else begin
-            waveform[5:0] = num;
+            waveform[5:0] = 6'd63 - num;
         end
         waveform = waveform << 6;      
     end
 end
+
+
 
 endmodule
