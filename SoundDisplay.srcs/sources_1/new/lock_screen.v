3a13c82e04efec1db3a6835f1faec223db16bd7b
Rayleigh47 <yanming8870@outlook.com>
2022-10-21 14:22:43 +0800

    merged main

---
+++ SoundDisplay.srcs/sources_1/new/lock_screen.v
@@ -0,0 +1,29 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 21.10.2022 13:07:29
+// Design Name: 
+// Module Name: lock_screen
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module lock_screen(
+    input CLK, //timeout
+    //input audio stuff,
+    output reg lock,
+    output reg [2:0] sequence
+    );
+endmodule
